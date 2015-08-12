2155e17c9866274ef6cef1ae9a7d7bb1815dbb8a
Glauber Costa <glommer@cloudius-systems.com>
2015-08-11 19:22:27 -0500

    row consumer: move consume_be to consumer.hh
    It will be reused by the continuous_data_consumer
    
    Signed-off-by: Glauber Costa <glommer@cloudius-systems.com>

--- sstables/consumer.hh
+++ sstables/consumer.hh
@@ -5,6 +5,13 @@
 
 #pragma once
 
+template<typename T>
+static inline T consume_be(temporary_buffer<char>& p) {
+    T i = net::ntoh(*unaligned_cast<const T*>(p.get()));
+    p.trim_front(sizeof(T));
+    return i;
+}
+
 class continuous_data_consumer {
 public:
     enum class proceed { yes, no };
