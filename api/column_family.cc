4e41b237f2427524bb899d0dee635684eaf08bbe
Glauber Costa <glommer@cloudius-systems.com>
2015-08-20 19:55:30 -0500

    api/column family: off-heap usage information
    We don't really support the concept of off-heap allocation, at least not in the
    same way that the JVM does. While we do have different between allocators -
    managed vs non-managed memory, and possibly others, I am not sure we should be
    overloading the original java-based statistics with this - specially since we
    don't actually intend to expose those as tunable choices.
    
    I will be happy to change that after some discussion if you guys disagree and
    reason about it satisfactorily.
    
    Signed-off-by: Glauber Costa <glommer@cloudius-systems.com>

--- api/column_family.cc
+++ api/column_family.cc
@@ -537,24 +537,18 @@ void set_column_family(http_context& ctx, routes& r) {
     });
 
     cf::get_bloom_filter_off_heap_memory_used.set(r, [] (std::unique_ptr<request> req) {
-        //TBD
-        //auto id = get_uuid(req->param["name"], ctx.db.local());
         return make_ready_future<json::json_return_type>(0);
     });
 
     cf::get_all_bloom_filter_off_heap_memory_used.set(r, [] (std::unique_ptr<request> req) {
-        //TBD
         return make_ready_future<json::json_return_type>(0);
     });
 
     cf::get_index_summary_off_heap_memory_used.set(r, [] (std::unique_ptr<request> req) {
-        //TBD
-        //auto id = get_uuid(req->param["name"], ctx.db.local());
         return make_ready_future<json::json_return_type>(0);
     });
 
     cf::get_all_index_summary_off_heap_memory_used.set(r, [] (std::unique_ptr<request> req) {
-        //TBD
         return make_ready_future<json::json_return_type>(0);
     });
 
