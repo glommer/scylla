08ef2d8ec02187dbfe1ef1b44e55fd267e16a50d
Glauber Costa <glauber@scylladb.com>
2016-09-19 09:33:39 -0400

    not to apply
    Signed-off-by: Glauber Costa <glauber@scylladb.com>

--- database.cc
+++ database.cc
@@ -1643,6 +1643,15 @@ database::setup_collectd() {
     ));
 
     _collectd.push_back(
+        scollectd::add_polled_metric(scollectd::type_instance_id("database"
+                , scollectd::per_cpu_plugin_instance
+                , "delay", "requests_blocked_memory")
+                , scollectd::make_typed(scollectd::data_type::GAUGE, [this] {
+                    return _dirty_memory_manager.region_group().blocked_for();
+                })
+    ));
+
+    _collectd.push_back(
         scollectd::add_polled_metric(scollectd::type_instance_id("memtables"
                 , scollectd::per_cpu_plugin_instance
                 , "bytes", "pending_flushes")
