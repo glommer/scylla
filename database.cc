9a3bee19513f3d13a16fa73c8ede31ae4235acf7
Glauber Costa <glauber@scylladb.com>
2016-11-16 00:10:48 -0500

    database: use commitlog_entry post_write function to write mutation
    Doing that will guarantee that the request can't be reordered, since the
    function will now be called right after the replay position is
    generated. Reorders will only happen if the commitlog itself reorders
    it, which shouldn't happen except due to bugs.
    
    Signed-off-by: Glauber Costa <glauber@scylladb.com>

--- database.cc
+++ database.cc
@@ -2584,21 +2584,20 @@ future<> database::do_apply(schema_ptr s, const frozen_mutation& m) {
                                  s->ks_name(), s->cf_name(), s->version()));
     }
     if (cf.commitlog() != nullptr) {
-        commitlog_entry_writer cew(s, m);
-        return cf.commitlog()->add_entry(uuid, cew).then([&m, this, s](auto rp) {
-            return this->apply_in_memory(m, s, rp).handle_exception([this, s, &m] (auto ep) {
-                try {
-                    std::rethrow_exception(ep);
-                } catch (replay_position_reordered_exception&) {
-                    // expensive, but we're assuming this is super rare.
-                    // if we failed to apply the mutation due to future re-ordering
-                    // (which should be the ever only reason for rp mismatch in CF)
-                    // let's just try again, add the mutation to the CL once more,
-                    // and assume success in inevitable eventually.
-                    dblog.debug("replay_position reordering detected");
-                    return this->apply(s, m);
-                }
-            });
+        commitlog_entry_writer cew(s, m, [this, s, &m] (auto rp) {
+            // Shouldn't ever see a reorder. The commitlog will call this in the order the replay
+            // positions are produced, and run_when_memory_available() guarantees FIFO ordering.
+            return this->apply_in_memory(m, s, rp);
+        });
+        return cf.commitlog()->add_entry(uuid, cew).discard_result().handle_exception([this, s, &m] (auto ep) {
+            try {
+                std::rethrow_exception(ep);
+            } catch (replay_position_reordered_exception&) {
+                // Shouldn't happen - see explanation above. If it does, let's just reinsert, since
+                // not doing so can lead to data corruption. But be vocal about it.
+                dblog.info("replay_position reordering detected");
+                return this->apply(s, m);
+            }
         });
     }
     return apply_in_memory(m, s, db::replay_position());
