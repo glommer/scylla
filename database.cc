04669c80e7c65da3b5842d844b4d5fe48c414d79
Glauber Costa <glauber@scylladb.com>
2016-11-23 13:05:30 -0500

    database: move reversion of virtual dirty state closer to update_cache.
    When we finish writing a memtable, we revert the dirty memory charges
    immediately. When we do that, dirty memory will grow back to what it
    was, and soon (we hope) will go down again when we release the requests
    for real.
    
    During that time, we may not accept new requests. Sealing can take a
    long time, specially in the face of Linux issues like the ones we have
    seen in the past. It also will take proportionally more time if the
    SSTables end up being small, which is a possibility in some scenarios.
    
    This patch changes the dirty_memory_manager so that the charges won't be
    reverted right after we finish the flush. Rather, we will hold on to it,
    and revert it right before we update the cache. We don't need to do it
    for all classes of memtable writes, because after we finish flushing,
    flush_one() will destroy the hashed element anyway.
    
    Signed-off-by: Glauber Costa <glauber@scylladb.com>

--- database.cc
+++ database.cc
@@ -1086,6 +1086,19 @@ column_family::try_flush_memtable_to_sstable(lw_shared_ptr<memtable> old) {
 
             trigger_compaction();
 
+            // Could pass the iterator to the seal functions, and avoid the need to search the
+            // unordered_map here. But this is supposed to be cheap and it is a lot less clutter in
+            // the method signatures. Also makes it optional and streaming memtables don't have to
+            // do it. Note that the number of entries in this hash is limited by the background
+            // flushes semaphore, so it'll always be small.
+            //
+            // In terms of releasing dirty memory, this is as far as we should go. After the cache
+            // starts to be updated the region in transferred over. We kind of assume there will be
+            // no deferring point between this and update cache transferring ownership. It's not
+            // that bad if it is so we wouldn't really protect against it, but without a deferring
+            // point we can guarantee that no request will see a spike in dirty memory between the
+            // release of our memory and the execution of a request.
+            dirty_memory_manager::from_region_group(old->region_group()).remove_from_flush_manager(&(old->region()));
             return update_cache(*old, newtab).then_wrapped([this, newtab, old] (future<> f) {
                 try {
                     f.get();
@@ -2556,7 +2569,7 @@ future<> dirty_memory_manager::flush_one(memtable_list& mtlist, semaphore_units<
     //
     // If we abandon size-driven flush and go with another flushing scheme that always guarantees
     // that we're picking from this region_group, we can simplify this.
-    dirty_memory_manager::from_region_group(region_group)._flush_manager.emplace(region, std::move(permit));
+    dirty_memory_manager::from_region_group(region_group).add_to_flush_manager(region, std::move(permit));
     auto fut = mtlist.seal_active_memtable(memtable_list::flush_behavior::immediate);
     return get_units(_background_work_flush_serializer, 1).then([this, fut = std::move(fut), region, region_group, schema] (auto permit) mutable {
         return std::move(fut).then_wrapped([this, region, region_group, schema] (auto f) {
