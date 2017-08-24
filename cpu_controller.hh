6b7755e96167ea4f035b57e1bd532de80ad6b5fa
Glauber Costa <glauber@scylladb.com>
2017-08-24 13:32:07 -0400

    factor out some of the controller code
    The control algorithm we are using for memtables have proven itself
    quite successful. We will very likely use the same for other processes,
    like compactions.
    
    Make the code a bit more generic, so that a new controller has to only
    set the desired parameters
    
    Signed-off-by: Glauber Costa <glauber@scylladb.com>

--- cpu_controller.hh
+++ cpu_controller.hh
@@ -24,23 +24,77 @@
 #include <seastar/core/timer.hh>
 #include <chrono>
 
-// Simple proportional controller to adjust shares of memtable/streaming flushes.
+// Simple proportional controller to adjust shares for processes for which a backlog can be clearly
+// defined.
 //
-// Goal is to flush as fast as we can, but not so fast that we steal all the CPU from incoming
-// requests, and at the same time minimize user-visible fluctuations in the flush quota.
+// Goal is to consume the backlog as fast as we can, but not so fast that we steal all the CPU from
+// incoming requests, and at the same time minimize user-visible fluctuations in the quota.
 //
-// What that translates to is we'll try to keep virtual dirty's firt derivative at 0 (IOW, we keep
-// virtual dirty constant), which means that the rate of incoming writes is equal to the rate of
-// flushed bytes.
+// What that translates to is we'll try to keep the backlog's firt derivative at 0 (IOW, we keep
+// backlog constant). As the backlog grows we increase CPU usage, decreasing CPU usage as the
+// backlog diminishes.
 //
-// The exact point at which the controller stops determines the desired flush CPU usage. As we
-// approach the hard dirty limit, we need to be more aggressive. We will therefore define two
+// The exact point at which the controller stops determines the desired CPU usage. As the backlog
+// grows and approach a maximum desired, we need to be more aggressive. We will therefore define two
 // thresholds, and increase the constant as we cross them.
 //
-//  1) the soft limit line
-//  2) halfway between soft limit and dirty limit
+// Doing that divides the range in three (before the first, between first and second, and after
+// second threshold), and we'll be slow to grow in the first region, grow normally in the second
+// region, and aggressively in the third region.
 //
 // The constants q1 and q2 are used to determine the proportional factor at each stage.
+struct backlog_cpu_controller {
+    struct disabled {
+        seastar::thread_scheduling_group *backup;
+    };
+
+    seastar::thread_scheduling_group* scheduling_group() {
+        return _current_scheduling_group;
+    }
+    float current_quota() const {
+        return _current_quota;
+    }
+private:
+    static constexpr float q1 = 0.01;
+    static constexpr float q2 = 0.2;
+    static constexpr float qmax = 1;
+
+    std::chrono::milliseconds _interval;
+    timer<> _update_timer;
+    seastar::thread_scheduling_group _scheduling_group;
+    seastar::thread_scheduling_group *_current_scheduling_group = nullptr;
+
+    float _current_quota = 0.0f;
+    float _first_threshold;
+    float _second_threshold;
+    float _maximum;
+
+    std::function<float()> _current_backlog;
+
+    void adjust();
+protected:
+    backlog_cpu_controller(disabled d) : _scheduling_group(std::chrono::nanoseconds(0), 0), _current_scheduling_group(d.backup) {}
+    backlog_cpu_controller(backlog_cpu_controller&&) = default;
+
+    backlog_cpu_controller(std::chrono::milliseconds interval, float ft, float st, float max, std::function<float()> backlog)
+        : _interval(interval)
+        , _update_timer([this] { adjust(); })
+        , _scheduling_group(std::chrono::milliseconds(1), _current_quota)
+        , _current_scheduling_group(&_scheduling_group)
+        , _first_threshold(ft)
+        , _second_threshold(st)
+        , _maximum(max)
+        , _current_backlog(std::move(backlog))
+    {
+         _update_timer.arm_periodic(_interval);
+    }
+};
+
+// memtable flush CPU controller.
+//
+// - First threshold is the soft limit line,
+// - Maximum is the point in which we'd stop consuming request,
+// - Second threshold is halfway between them.
 //
 // Below the soft limit, we are in no particular hurry to flush, since it means we're set to
 // complete flushing before we a new memtable is ready. The quota is dirty * q1, and q1 is set to a
@@ -51,39 +105,18 @@
 //
 // In the second half, we're getting close to the hard dirty limit so we increase the slope and
 // become more responsive, up to a maximum quota of qmax.
-//
-// For now we'll just set them in the structure not to complicate the constructor. But q1, q2 and
-// qmax can easily become parameters if we find another user.
-class flush_cpu_controller {
-    static constexpr float hard_dirty_limit = 0.50;
-    static constexpr float q1 = 0.01;
-    static constexpr float q2 = 0.2;
-    static constexpr float qmax = 1;
-
-    float _current_quota = 0.0f;
-    float _goal;
-    std::function<float()> _current_dirty;
-    std::chrono::milliseconds _interval;
-    timer<> _update_timer;
-
-    seastar::thread_scheduling_group _scheduling_group;
-    seastar::thread_scheduling_group *_current_scheduling_group = nullptr;
-
-    void adjust();
+class flush_cpu_controller : public backlog_cpu_controller {
+    static constexpr float hard_dirty_limit = 1.0f;
 public:
-    seastar::thread_scheduling_group* scheduling_group() {
-        return _current_scheduling_group;
-    }
-    float current_quota() const {
-        return _current_quota;
-    }
-
-    struct disabled {
-        seastar::thread_scheduling_group *backup;
-    };
-    flush_cpu_controller(disabled d) : _scheduling_group(std::chrono::nanoseconds(0), 0), _current_scheduling_group(d.backup) {}
-    flush_cpu_controller(std::chrono::milliseconds interval, float soft_limit, std::function<float()> current_dirty);
+    flush_cpu_controller(backlog_cpu_controller::disabled d) : backlog_cpu_controller(std::move(d)) {}
     flush_cpu_controller(flush_cpu_controller&&) = default;
+    flush_cpu_controller(std::chrono::milliseconds interval, float soft_limit, std::function<float()> current_dirty)
+        : backlog_cpu_controller(std::move(interval),
+                                 soft_limit,
+                                 soft_limit + (hard_dirty_limit - soft_limit) / 2,
+                                 hard_dirty_limit,
+                                 std::move(current_dirty))
+    {}
 };
 
 
