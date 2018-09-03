b53c99b87da71efcaf092dde23d5761355412f55
Takuya ASADA <syuu@scylladb.com>
2018-09-03 22:23:44 +0900

    build: compress relocatable package
    Since debian packaging system requires source package to compress tar
    file, so let's use .gz compression.
    
    Signed-off-by: Takuya ASADA <syuu@scylladb.com>

--- configure.py
+++ configure.py
@@ -1286,7 +1286,7 @@ with open(buildfile, 'w') as f:
         f.write(textwrap.dedent('''\
             build build/{mode}/iotune: copy seastar/build/{mode}/apps/iotune/iotune
             ''').format(**locals()))
-        f.write('build build/$mode/scylla-package.tar: package build/{mode}/scylla build/{mode}/iotune\n'.format(**locals()))
+        f.write('build build/$mode/scylla-package.tar.gz: package build/{mode}/scylla build/{mode}/iotune\n'.format(**locals()))
         f.write('    mode = {mode}\n'.format(**locals()))
         f.write('rule libdeflate.{mode}\n'.format(**locals()))
         f.write('    command = make -C libdeflate BUILD_DIR=../build/{mode}/libdeflate/ CFLAGS="{libdeflate_cflags}" CC={args.cc}\n'.format(**locals()))
