513eca670dbe08ef5896ec8fcd82182536be53e9
Takuya ASADA <syuu@scylladb.com>
2018-10-15 09:37:35 +0000

    configure.py: add SCYLLA-RELEASE-FILE/SCYLLA-VERSION-FILE targets
    To re-generate scylla version files when it removed, since these files
    required for relocatable package.
    
    Signed-off-by: Takuya ASADA <syuu@scylladb.com>
    (cherry picked from commit 6e1617d71cd7e3a2789445f42b43e9ff5d45574e)
    Signed-off-by: Glauber Costa <glauber@scylladb.com>

--- configure.py
+++ configure.py
@@ -1286,7 +1286,7 @@ with open(buildfile, 'w') as f:
         f.write(textwrap.dedent('''\
             build build/{mode}/iotune: copy seastar/build/{mode}/apps/iotune/iotune
             ''').format(**locals()))
-        f.write('build build/{mode}/scylla-package.tar.gz: package build/{mode}/scylla build/{mode}/iotune\n'.format(**locals()))
+        f.write('build build/{mode}/scylla-package.tar.gz: package build/{mode}/scylla build/{mode}/iotune build/SCYLLA-RELEASE-FILE build/SCYLLA-VERSION-FILE\n'.format(**locals()))
         f.write('    mode = {mode}\n'.format(**locals()))
         f.write('rule libdeflate.{mode}\n'.format(**locals()))
         f.write('    command = make -C libdeflate BUILD_DIR=../build/{mode}/libdeflate/ CFLAGS="{libdeflate_cflags}" CC={args.cc}\n'.format(**locals()))
@@ -1307,4 +1307,9 @@ with open(buildfile, 'w') as f:
             description = CLEAN
         build clean: clean
         default {modes_list}
+        ''').format(modes_list=' '.join(build_modes), **globals()))
+    f.write(textwrap.dedent('''\
+        rule scylla_version_gen
+            command = ./SCYLLA-VERSION-GEN
+        build build/SCYLLA-RELEASE-FILE build/SCYLLA-VERSION-FILE: scylla_version_gen
         ''').format(modes_list=' '.join(build_modes), **globals()))
